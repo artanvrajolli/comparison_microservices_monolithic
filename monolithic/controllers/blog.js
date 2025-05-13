fcc1bd021108d1c73b07355e984d1ddbd304ffe0
ArtanVrajolli <artanvrajolli@gmail.com>
2025-05-13 23:38:22 +0200

    return array direct in response

--- monolithic/controllers/blog.js
+++ monolithic/controllers/blog.js
@@ -3,20 +3,44 @@ const { Blog } = require('../models');
 exports.getBlogsWithDetails = async (req, res) => {
     try {
         const blogs = await Blog.find()
-            .populate('author', 'username email')
+            .populate({
+                path: 'author',
+                select: 'username email',
+                transform: doc => ({
+                    id: doc._id,
+                    ...doc,
+                    _id: undefined,
+                    __v: undefined
+                })
+            })
             .populate({
                 path: 'comments',
+                transform: doc => ({
+                    id: doc._id,
+                    ...doc,
+                    _id: undefined,
+                    __v: undefined
+                }),
                 populate: {
                     path: 'user',
-                    select: 'username email'
+                    select: 'username email',
+                    transform: doc => ({
+                        id: doc._id,
+                        ...doc,
+                        _id: undefined,
+                        __v: undefined
+                    })
                 }
-            });
+            })
+            .lean()
+            .transform(docs => docs.map(doc => ({
+                id: doc._id,
+                ...doc,
+                _id: undefined,
+                __v: undefined
+            })));
         
-        res.status(200).json({
-            success: true,
-            count: blogs.length,
-            data: blogs,
-        });
+        res.status(200).json(blogs);
     } catch (error) {
         res.status(500).json({
             success: false,
